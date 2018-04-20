3bc1ce6c0b5cbf36ef5716dea06746662f34dd9c
Wesley Wiedenmeier <wesley.wiedenmeier@gmail.com>
2018-04-20 04:19:56 -0500

    Added remote event triggered callback system #10

--- lizard/server/remote_event.py
+++ lizard/server/remote_event.py
@@ -1,6 +1,8 @@
 import contextlib
 import threading
+import queue
 
+from lizard.events import EventStatus
 
 REMOTE_EVENTS = None
 REMOTE_EVENTS_LOCK = threading.Lock()
@@ -26,6 +28,9 @@ class RemoteEvents(object):
     def __init__(self):
         """RemoteEvents init"""
         self._remote_event_map = {}
+        self._callback_map = {}
+        self._callback_queue = queue.Queue()
+        self.start_callback_worker()
 
     def register_client(self, client_id):
         """
@@ -36,6 +41,18 @@ class RemoteEvents(object):
         if client_id in self._remote_event_map:
             raise ValueError("Client already exists")
         self._remote_event_map[client_id] = {}
+        self._callback_map[client_id] = {}
+
+    def register_callback_func(self, client_id, event_id, func):
+        """
+        Register a callback function
+        :client_id: client uuid
+        :event_id: event uuid
+        :func: function call to register, must accept event data as only arg
+        """
+        if event_id not in self._callback_map[client_id]:
+            self._callback_map[client_id][event_id] = []
+        self._callback_map[client_id][event_id].append(func)
 
     def store_event(self, client_id, event_id, event_props):
         """
@@ -48,3 +65,21 @@ class RemoteEvents(object):
         if client_id not in self._remote_event_map:
             raise ValueError("Client does not exist")
         self._remote_event_map[client_id][event_id] = event_props
+        final_states = (EventStatus.SUCCESS.value, EventStatus.FAILURE.value)
+        if event_props['status'] in final_states:
+            for func in self._callback_map[client_id].get(event_id, []):
+                callback_data = {'funcall': func, 'event_data': event_props}
+                self._callback_queue.put_nowait(callback_data)
+
+    def start_callback_worker(self):
+        """Worker for handling remote event triggered callbacks"""
+
+        def callback_worker():
+            while True:
+                callback_data = self._callback_queue.get(block=True)
+                callback_data['funcall'](callback_data['event_data'])
+                self._callback_queue.task_done()
+
+        self._callback_worker_thread = threading.Thread(target=callback_worker)
+        self._callback_worker_thread.daemon = True
+        self._callback_worker_thread.start()
