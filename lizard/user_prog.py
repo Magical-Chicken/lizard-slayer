aa84cb63c4a3d6c519e826edf3f40f2de4ac6ed6
Wesley Wiedenmeier <wesley.wiedenmeier@gmail.com>
2018-04-22 14:15:38 -0500

    Added option to use UserProg independant of actual user prog json

--- lizard/user_prog.py
+++ lizard/user_prog.py
@@ -18,23 +18,27 @@ ADDITIONAL_BUILD_FILES = ('Makefile', 'setup.py')
 class UserProg(object):
     """A user program"""
 
-    def __init__(self, name, checksum, data_file, build_dir=None):
+    def __init__(
+            self, name, checksum, data_file, build_dir=None,
+            ignore_data_file=False):
         """
         UserProg init
         :name: human readable program name
         :checksum: checksum of data file, and id key
         :data_file: path to json program definition blob
         :build_dir: if provided, directory to build code in
+        :ignore_data_file: if true, ignore json data file
         """
         self.ready = False
         self.name = name
         self.checksum = checksum
         self.build_dir = build_dir
         self.data_file = data_file
-        with open(self.data_file, 'r') as fp:
-            self.data = json.load(fp)
+        if not ignore_data_file:
+            with open(self.data_file, 'r') as fp:
+                self.data = json.load(fp)
 
-    def _unpack(self):
+    def unpack(self):
         """unpack program files and set up build dir structure"""
         LOG.debug('Extracting user program code')
         for code_key, filename in PROGRAM_SOURCE_FILE_NAMES.items():
@@ -42,7 +46,14 @@ class UserProg(object):
             path = os.path.join(self.build_dir, filename)
             with open(path, 'w') as fp:
                 fp.write(code)
-        for build_file in ADDITIONAL_BUILD_FILES:
+
+    def copy_build_files(self, build_files=ADDITIONAL_BUILD_FILES):
+        """
+        copy build files from data/build_files into build dir
+        :build_files: names of files to copy
+        """
+        LOG.debug('Copying additional build files')
+        for build_file in build_files:
             resource_path = os.path.join(
                 PROGRAM_DATA_DIRNAME, 'build_files', build_file)
             data = pkgutil.get_data('lizard', resource_path)
@@ -50,16 +61,23 @@ class UserProg(object):
             with open(path, 'wb') as fp:
                 fp.write(data)
 
-    def build(self, cuda_bin=None, include_path=None):
+    def build(
+            self, cuda_bin=None, include_path=None, unpack=True,
+            build_files=ADDITIONAL_BUILD_FILES):
         """
         build the shared object and python wrapper module
         note that the build dir must exist and have user prog kernel in it
         :cuda_bin: path to cuda tools bin
         :include_path: path to cuda include dir
+        :unpack: if true, unpack program json
+        :build_files: names of files to copy
         """
         if not self.build_dir or not os.path.isdir(self.build_dir):
             raise ValueError("Build dir not set up")
-        self._unpack()
+        if unpack:
+            self.unpack()
+        if build_files:
+            self.copy_build_files(build_files=build_files)
         make_cmd = ['make', '-C', self.build_dir]
         if cuda_bin is not None:
             nvcc_path = os.path.join(cuda_bin, 'nvcc')
