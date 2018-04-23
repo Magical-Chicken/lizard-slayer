ffb8b58ec6af673fbf554ec1daaf65ea40bf82be
Wesley Wiedenmeier <wesley.wiedenmeier@gmail.com>
2018-04-23 01:33:51 -0500

    Added hardware discovery cuda code and header

---
+++ lizard/data/hw_discovery/program.h
@@ -0,0 +1,15 @@
+#ifndef _SCAN_H
+#define _SCAN_H
+
+struct gpu_props {
+    int gpu_index, comp_level_major, comp_level_minor,
+        sm_count, max_sm_threads, max_sm_blocks, max_block_size,
+        max_total_threads, max_total_blocks;
+    char name[256];
+};
+
+int get_num_gpus();
+
+void get_gpu_data(int gpu_index, struct gpu_props *props);
+
+#endif
