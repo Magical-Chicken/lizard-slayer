974495a969251e34574d51403e5b9337e9096867
Wesley Wiedenmeier <wesley.wiedenmeier@gmail.com>
2018-04-25 12:36:11 -0500

    Added auxilary fields system for dynamically alloced userprog data

--- lizard/data/build_files/resources.py
+++ lizard/data/build_files/resources.py
@@ -26,16 +26,33 @@ def b64_json_dec(encoded):
 class EncodableStructure(ctypes.Structure):
     """Base class for encodable ctypes structures"""
     _fields_ = []
+    aux_field_names = []
 
     def encode(self):
         """encode as b64 encoded json string"""
         data = {}
         for field, _ in self._fields_:
+            if field in self.aux_field_names:
+                continue
             data[field] = getattr(self, field)
+        if len(self.aux_field_names) > 0:
+            data['aux'] = self.encode_aux()
         return b64_json_enc(data)
 
     def decode(self, encoded):
         """decode from b64 encoded json string"""
         data = b64_json_dec(encoded)
         for field, _ in self._fields_:
+            if field in self.aux_field_names:
+                continue
             setattr(self, field, data[field])
+        if len(self.aux_field_names) > 0:
+            self.decode_aux(data['aux'])
+
+    def encode_aux(self):
+        """encode auxilary data that cannot be auto encoded"""
+        raise NotImplementedError
+
+    def decode_aux(self, encoded):
+        """decode auxiliry data that cannot be auto encoded"""
+        raise NotImplementedError
