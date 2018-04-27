2df12db81f9d8e1b89090a76dfe65a0432960612
Wesley Wiedenmeier <wesley.wiedenmeier@gmail.com>
2018-04-27 04:34:56 -0500

    Implemented dynamic encoding and decoding system for demo prog

--- config/user_programs/demo-ctypes/python_funcs.py
+++ config/user_programs/demo-ctypes/python_funcs.py
@@ -9,12 +9,66 @@ class GlobalState(user_prog_resources.EncodableStructure):
     ]
     aux_field_names = ('values',)
 
+    def encode_dynamic(self, global_params):
+        """
+        encode dynamic alloced data that cannot be auto encoded
+        :global_params: global parameters object
+        :returns: b64 encoded json
+        """
+        if self.values:
+            values = []
+            for i in range(global_params.dims):
+                values.append(self.values[i])
+        else:
+            values = self.values_aux
+        return user_prog_resources.b64_json_enc({'values': values})
+
+    def decode_dynamic(self, encoded, global_params):
+        """
+        decode dynamic alloced data that cannot be auto encoded
+        :global_params: global parameters object
+        :encoded: b64 encoded json
+        """
+        decoded = user_prog_resources.b64_json_dec(encoded)
+        if self.values:
+            for i in range(global_params.dims):
+                self.values[i] = decoded['values'][i]
+        else:
+            self.values_aux = decoded['values']
+
 
 class AggregationResult(user_prog_resources.EncodableStructure):
     _fields_ = [
         ('values', POINTER(c_int)),
     ]
     aux_field_names = ('values',)
+
+    def encode_dynamic(self, global_params):
+        """
+        encode dynamic alloced data that cannot be auto encoded
+        :global_params: global parameters object
+        :returns: b64 encoded json
+        """
+        if self.values:
+            values = []
+            for i in range(global_params.dims):
+                values.append(self.values[i])
+        else:
+            values = self.values_aux
+        return user_prog_resources.b64_json_enc({'values': values})
+
+    def decode_dynamic(self, encoded, global_params):
+        """
+        decode dynamic alloced data that cannot be auto encoded
+        :global_params: global parameters object
+        :encoded: b64 encoded json
+        """
+        decoded = user_prog_resources.b64_json_dec(encoded)
+        if self.values:
+            for i in range(global_params.dims):
+                self.values[i] = decoded['values'][i]
+        else:
+            self.values_aux = decoded['values']
 
 
 class Dataset(user_prog_resources.EncodableStructure):
@@ -23,6 +77,35 @@ class Dataset(user_prog_resources.EncodableStructure):
         ('points', POINTER(POINTER(c_int))),
     ]
     aux_field_names = ('points',)
+
+    def encode_dynamic(self, global_params):
+        """
+        encode dynamic alloced data that cannot be auto encoded
+        :global_params: global parameters object
+        :returns: b64 encoded json
+        """
+        if self.points:
+            points = [[]] * self.num_points
+            for i in range(self.num_points):
+                for j in range(global_params.dims):
+                    points[i].append(self.points[i][j])
+        else:
+            points = self.points_aux
+        return user_prog_resources.b64_json_enc({'points': points})
+
+    def decode_dynamic(self, encoded, global_params):
+        """
+        decode dynamic alloced data that cannot be auto encoded
+        :global_params: global parameters object
+        :encoded: b64 encoded json
+        """
+        decoded = user_prog_resources.b64_json_dec(encoded)
+        if self.values:
+            for i in range(self.num_points):
+                for j in range(global_params.dims):
+                    self.points[i][j] = decoded['points'][i][j]
+        else:
+            self.points_aux = decoded['points']
 
 
 class GlobalParams(user_prog_resources.EncodableStructure):
--- lizard/user_prog_resources.py
+++ lizard/user_prog_resources.py
@@ -26,33 +26,49 @@ def b64_json_dec(encoded):
 class EncodableStructure(ctypes.Structure):
     """Base class for encodable ctypes structures"""
     _fields_ = []
-    aux_field_names = []
+    dynamic_alloc_fields = []
 
-    def encode(self):
-        """encode as b64 encoded json string"""
+    def encode(self, global_params):
+        """
+        encode as b64 encoded json string
+        :global_params: global parameters object
+        :returns: b64 encoded json
+        """
         data = {}
         for field, _ in self._fields_:
-            if field in self.aux_field_names:
+            if field in self.dynamic_alloc_fields:
                 continue
             data[field] = getattr(self, field)
         if len(self.aux_field_names) > 0:
-            data['aux'] = self.encode_aux()
+            data['dynamic'] = self.encode_dynamic(global_params)
         return b64_json_enc(data)
 
-    def decode(self, encoded):
-        """decode from b64 encoded json string"""
+    def decode(self, encoded, global_params):
+        """
+        decode from b64 encoded json string
+        :global_params: global parameters object
+        :encoded: b64 encoded json
+        """
         data = b64_json_dec(encoded)
         for field, _ in self._fields_:
             if field in self.aux_field_names:
                 continue
             setattr(self, field, data[field])
         if len(self.aux_field_names) > 0:
-            self.decode_aux(data['aux'])
+            self.decode_dynamic(data['dynamic'], global_params)
 
-    def encode_aux(self):
-        """encode auxilary data that cannot be auto encoded"""
+    def encode_dynamic(self, global_params):
+        """
+        encode dynamic alloced data that cannot be auto encoded
+        :global_params: global parameters object
+        :returns: b64 encoded json
+        """
         raise NotImplementedError
 
-    def decode_aux(self, encoded):
-        """decode auxiliry data that cannot be auto encoded"""
+    def decode_dynamic(self, encoded, global_params):
+        """
+        decode dynamic alloced data that cannot be auto encoded
+        :global_params: global parameters object
+        :encoded: b64 encoded json
+        """
         raise NotImplementedError
