60d0d9a8bfcd2d61067137b9fb441d6f7c5a6e8a
Wesley Wiedenmeier <wesley.wiedenmeier@gmail.com>
2018-04-30 04:32:24 -0500

    Added run_using_runner_module() to run user program

--- config/user_programs/demo_ctypes/runner.py
+++ config/user_programs/demo_ctypes/runner.py
@@ -9,30 +9,30 @@ except ImportError:
     import python_funcs
 
 
-def init_global_params(global_params_conf):
+def init_global_params(run_settings):
     """
     initialize global parameters
-    :global_params_conf: global params config dict, must contain
+    :run_settings: run settings dict, must contain
         - dims
         - max_iterations
     :returns: GlobalParams object
     """
     global_params = python_funcs.GlobalParams()
-    global_params.dims = global_params_conf['dims']
-    global_params.max_iterations = global_params_conf['max_iterations']
+    global_params.dims = run_settings['dims']
+    global_params.max_iterations = run_settings['max_iterations']
     return global_params
 
 
-def init_dataset(global_params, dataset_conf):
+def init_dataset(global_params, run_settings):
     """
     initialize dataset
     :global_params: global parameters object
-    :dataset_conf: dataset config dict, must contain
+    :run_settings: run settings dict, must contain
         - input_file
     :returns: Dataset object
     """
     dataset = python_funcs.Dataset()
-    with open(dataset_conf['input_file'], 'r') as fp:
+    with open(run_settings['input_file'], 'r') as fp:
         lines = fp.readlines()
     dataset.num_points = len(lines)
     dataset.init_aux_structures(global_params)
@@ -42,3 +42,14 @@ def init_dataset(global_params, dataset_conf):
         for d_idx in range(global_params.dims):
             points_ref[l_idx][d_idx] = line_vals[d_idx]
     return dataset
+
+
+def print_result(global_params, end_global_state):
+    """
+    print results from end global state
+    :global_params: global parameters object
+    :end_global_state: end global state string
+    """
+    global_state = python_funcs.GlobalState()
+    global_state.decode(end_global_state, global_params)
+    print(global_state.values_aux)
--- lizard/runtime_helper.py
+++ lizard/runtime_helper.py
@@ -1,3 +1,4 @@
+import importlib
 import os
 import requests
 import time
@@ -22,3 +23,26 @@ def poll_for_event_complete(
             break
         time.sleep(poll_freq)
     return data
+
+
+def run_using_runner_module(
+        runner_module_name, run_settings, server_address, checksum):
+    """
+    run a program using a runner module, see demo program for example
+    :runner_module_name: runner module import name
+    :run_settings: dict of run settings needed by runner
+    :server_address: full uri for server
+    :checksum: program checksum/identifier
+    """
+    runner_mod = importlib.import_module(runner_module_name)
+    global_params = runner_mod.init_global_params(run_settings)
+    dataset = runner_mod.init_dataset(global_params, run_settings)
+    event_data = {
+        'global_params_enc': global_params.encode(None),
+        'dataset_enc': dataset.encode(global_params),
+    }
+    path = os.path.join(server_address, 'runtimes', checksum)
+    req_data = requests.post(path, json=event_data).json()
+    result_data = poll_for_event_complete(server_address, req_data['event_id'])
+    end_global_state = result_data['result']['end_global_state']
+    runner_mod.print_result(global_params, end_global_state)
